9c4f83c7a2b4cf199320155001963289a7fa8c67
Robert Pearce <robertwaltonpearce@gmail.com>
2014-05-19 08:58:04 -0400

    Added class4 in-class before and after example/walk-through

---
+++ in-class/class4/after/app.js
@@ -0,0 +1,47 @@
+;(function() {
+  var questionaireInput = document.querySelector('.questionaire').getElementsByTagName('input')[0];
+  var todoInput = document.querySelector('.todo-input');
+
+  var handleQuestionaireKeyup = function(e) {
+    e.preventDefault();
+    var text = e.target.value;
+    if (e.keyCode === 13 && text.length > 0) {
+      questionaireContainer = document.querySelector('.questionaire')
+      todoContainer = document.querySelector('.todo')
+      questionaireContainer.className += ' hidden';
+      todoContainer.className = 'todo';
+      document.querySelector('.todo-title').innerHTML = text;
+    }
+  };
+
+  var handleCheckboxDone = function(e) {
+    console.log(e);
+    if (e.target.checked) {
+      console.log('checked');
+    }
+  };
+
+  var handleTodoKeyup = function(e) {
+    e.preventDefault();
+    var text = e.target.value
+    if (e.keyCode === 13 && text.length > 0) {
+      var todoListItem = document.createElement('li'),
+          todoListItemDone = document.createElement('input'),
+          todoList = document.querySelector('.todo-list');
+
+      todoListItemDone.type = 'checkbox';
+      todoListItemDone.className = 'todo-list-item-done';
+      todoListItemDone.addEventListener('click', handleCheckboxDone); // NOTE: this listener isn't working!
+
+      todoListItem.appendChild(todoListItemDone);
+      todoListItem.innerHTML += text;
+
+      todoList.appendChild(todoListItem);
+
+      e.target.value = '';
+    }
+  };
+
+  questionaireInput.addEventListener('keyup', handleQuestionaireKeyup)
+  todoInput.addEventListener('keyup', handleTodoKeyup)
+})();
